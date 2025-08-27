30933820286e75afc5097262c8db6250f3828300
juliancorrea386 <juliancorrea386@gmail.com>
2025-08-27 10:03:57 -0500

    Ajustes

--- cotizaciones_fronend/src/App.jsx
+++ cotizaciones_fronend/src/App.jsx
@@ -1,4 +1,5 @@
 // App.jsx
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ProductosPage from "./pages/ProductosPage";
