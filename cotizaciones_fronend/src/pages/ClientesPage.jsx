0692669564cd516cd97e7b078f2f3b0d376ea23d
juliancorrea386 <juliancorrea386@gmail.com>
2025-09-03 14:54:46 -0500

    Ajuste clientePage

--- cotizaciones_fronend/src/pages/ClientesPage.jsx
+++ cotizaciones_fronend/src/pages/ClientesPage.jsx
@@ -15,7 +15,6 @@ export default function ClientesPage() {
     }, []);
 
     const fetchClientes = async (params = {}) => {
-        console.log("Fetching clientes with params:", params);
         try {
             const data = await clientesService.listar(params);
             setClientes(data);
